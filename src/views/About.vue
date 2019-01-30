7f46b3d51e9cfbad4045fddd36224a159d333e64
Manuel Saelices <msaelices@gmail.com>
2019-01-30 14:29:53 +0100

    Result of executing `vue invoke vue-cli-plugin-nativescript-vue` choosing default choices, with a brand new project option selected.

---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-page">
+    <div class="w-container">
+      <span>This is an about page</span>
+    </div>
+  </div>
+</template>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="w-page">
+    <div class="w-container">
+      <img src="~/assets/logo.png" alt="logo">
+      <HelloWorld :msg="msg"/>
+    </div>
+  </div>
+</template>
+<script>
+  import HelloWorld from 'components/HelloWorld';
+
+  export default {
+    name: 'home',
+    components: {
+      HelloWorld
+    },
+    data() {
+      return {
+        msg: 'Mode=' + TNS_APP_MODE + ' and Platform=' + TNS_APP_PLATFORM
+      };
+    }
+  };
+
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  img {
+    height: 20%;
+    width: 20%;
+    display: block;
+    margin: auto;
+    margin-top: 4em;
+  }
+</style>
