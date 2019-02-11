81f8c00526c6a814b1f025218c529858ad2634ed
Manuel Saelices <msaelices@gmail.com>
2019-02-11 12:06:46 +0100

    Just after running `vue invoke vue-cli-plugin-nativescript-vue` in the `custom-loader` branch

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="w-page">
+    <div class="w-container">
+      <img src="~/assets/logo.png" alt="logo" height="20%" width="20%">
+      <HelloWorld :msg="msg"/>
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  import HelloWorld from 'components/HelloWorld';
+  export default {
+    name: 'home',
+    components: {
+      HelloWorld
+    },
+    data() {
+      return {
+        msg: 'Mode=' + TNS_APP_MODE + ' and Platform=' + TNS_APP_PLATFORM,
+      };
+    },
+  };
 </script>
-
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .w-page {
+    height: 100%;
+    width: 100%;
+  }
+
+  .w-navbar {
+    position: fixed;
+    z-index: 10000;
+    height: 3em;
+    width: 100%;
+    top: 0px;
+    left: 0px;
+    margin: auto;
+    list-style: none;
+
+    display: flex;
+    align-items: center;
+    padding: 0 10px;
+
+    -webkit-box-shadow: -8px 8px 6px -7px #999;
+    -moz-box-shadow: -8px 8px 6px -7px #999;
+    box-shadow: -8px 8px 6px -7px #999;
+  }
+
+  .w-navbar .w-title {
+    margin-left: auto;
+    margin-right: auto;
+  }
+  
+  .w-container {
+    height: 100%;
+    width: 100%;
+    padding-top: 3em;
+    position: relative;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    justify-content: top;
+    align-items: center;
+  }
+
+  .w-container .w-button {
+    width: 50%;
+    height: 2em;
+    margin: .25em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #d7d7d7;
+    border-width: 0px;
+    font-weight: 600;
+    border-radius: 3px;
+  }
 </style>
