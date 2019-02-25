ec1e32d6a566252548326764ea9e87d9e7523327
Manuel Saelices <msaelices@gmail.com>
2019-02-25 12:47:21 +0100

    App after invoking plugin with tslint fixes.

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,108 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+<template web>
+  <div class="w-page">
+    <nav>
+      <ul class="w-navbar">
+        <li class="w-title" :text="navbarTitle">{{navbarTitle}}</li>
+      </ul>
+    </nav>
+    <div class="w-container">
+      <router-link tag="button" class="w-button" id="homeButton" to="/">Home</router-link>
+      <router-link tag="button" class="w-button" id="aboutButton" to="/about">About</router-link>
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
+<template native>
+  <Page>
+    <ActionBar :title="navbarTitle"/>
+    <GridLayout rows="auto, auto">
+      <Button text="Home" @tap="goToHomePage" row="0"/>
+      <Button text="About" @tap="goToAboutPage" row="1"/>
+    </GridLayout>
+  </Page>
+</template>
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+  import Home from '~/views/Home.vue';
+  import About from '~/views/About.vue';
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  @Component({
+    name: 'home',
+  })
+  export default class App extends Vue {
+    private navbarTitle: string = `App.vue`;
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    public goToHomePage() {
+      Vue.prototype.$navigateTo(Home);
+    }
+
+    public goToAboutPage() {
+      Vue.prototype.$navigateTo(About);
+    }
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style web>
+  .w-page {
+    height: 100%;
+    width: 100%;
+  }
+
+  .w-navbar {
+    position: fixed;
+    z-index: 10000;
+    height: 3em;
+    width: 100%;
+    top: 0px;
+    left: 0px;
+    margin: auto;
+    list-style: none;
+
+    display: flex;
+    align-items: center;
+    padding: 0 10px;
+
+    -webkit-box-shadow: -8px 8px 6px -7px #999;
+    -moz-box-shadow: -8px 8px 6px -7px #999;
+    box-shadow: -8px 8px 6px -7px #999;
+  }
+
+  .w-navbar .w-title {
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .w-container {
+    height: 100%;
+    width: 100%;
+    padding-top: 3em;
+    position: relative;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    justify-content: top;
+    align-items: center;
+  }
+
+  .w-container .w-button {
+    width: 50%;
+    height: 2em;
+    margin: .25em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #d7d7d7;
+    border-width: 0px;
+    font-weight: 600;
+    border-radius: 3px;
+  }
+</style>
+<style native>
+  ActionBar {
+    background-color: #42b983;
+    color: white;
+  }
 </style>
