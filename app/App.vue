6d9ebfb8892f4cb003fd4225c95e9cef8ed5dac7
Manuel Saelices <msaelices@gmail.com>
2019-03-11 09:59:54 +0100

    Generated stuff by the vue-cli-plugin-nativescript-vue with `Native Only` option

---
+++ app/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <Page>
+    <ActionBar :title="navbarTitle"/>
+    <GridLayout rows="auto, auto">
+      <HelloWorld :msg="msg"/>
+    </GridLayout>
+  </Page>
+</template>
+<script>
+  import HelloWorld from '~/components/HelloWorld';
+
+  const { VUE_APP_MODE, VUE_APP_PLATFORM } = process.env;
+
+  export default {
+    components: {
+      HelloWorld,
+    },
+    data() {
+      return {
+        navbarTitle: `App.vue`,
+        msg: `Mode=${VUE_APP_MODE} and Platform=${VUE_APP_PLATFORM}`,
+      };
+    },
+    methods: {
+    }
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style web>
+  @import '~styles/style-one';
+
+  .w-page {
+    height: 100%;
+    width: 100%;
+  }
+</style>
+<style native>
+  @import '~styles/style-one';
+</style>
---
+++ app/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <Page>
+    <ActionBar :title="navbarTitle"/>
+    <GridLayout>
+      <Label text="This is an about page" textWrap="true" horizontalAlignment="center" verticalAlignment="center"/>
+    </GridLayout>
+  </Page>
+</template>
+<script>
+
+  export default {
+    data() {
+      return {
+        navbarTitle: `About.vue`
+      };
+    }
+  };
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  @import '~styles/style-two';
+  @import '~styles/style-one';
+</style>
