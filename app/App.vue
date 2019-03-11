900bf6f6deb7fc1ec0804c95d40589ae6e4e6be7
Manuel Saelices <msaelices@gmail.com>
2019-03-11 10:55:35 +0100

    Generated stuff by the vue-cli-plugin-nativescript-vue with `Native Only` option

---
+++ app/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <Page>
+    <ActionBar :title="navbarTitle"/>
+    <GridLayout rows="auto, auto">
+      <Button text="Home" @tap="goToHomePage" row="0"/>
+      <Button text="About" @tap="goToAboutPage" row="1"/>
+    </GridLayout>
+  </Page>
+</template>
+<script>
+  import Home from '~/views/Home';
+  import About from '~/views/About';
+
+  const { VUE_APP_MODE } = process.env;
+
+  export default {
+    data() {
+      return {
+        navbarTitle: `App.vue`,
+      };
+    },
+    methods: {
+      goToHomePage() {
+        this.$navigateTo(Home);
+      },
+      goToAboutPage() {
+        VUE_APP_MODE == 'web' ? this.$router.push('about') : this.$navigateTo(About);
+      }
+    }
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style web>
+  @import '~styles/style-one';
+
+  .w-page {
+    height: 100%;
+    width: 100%;
+  }
+</style>
+<style native>
+  @import '~styles/style-one';
+</style>
