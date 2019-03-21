86b8abdfb93c26f7df86046cbf5f06814ea6a5d4
Manuel Saelices <msaelices@gmail.com>
2019-03-21 13:49:04 +0100

    Resulting app after invoking the plugin.

--- src/router.js
+++ src/router.js
@@ -1,12 +1,11 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '~/views/Home.vue';
+import About from '~/views/About.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
+export const options = {
   routes: [
     {
       path: '/',
@@ -16,10 +15,8 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: About,
     }
   ]
-})
+};
+export default new Router(options);
