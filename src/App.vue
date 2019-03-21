7ffb3e12b837f5edfdfdb4242ac29e6809e1e5a0
Manuel Saelices <msaelices@gmail.com>
2019-03-21 13:13:17 +0100

    Resulting app after invoking the plugin.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template web>
+  <div class="w-page">
+    <div class="w-container">
+      <img src="~/assets/logo.png" alt="logo" height="20%" width="20%">
+      <HelloWorld :msg="msg"/>
+    </div>
   </div>
 </template>
-
+<template native>
+  <Page>
+    <ActionBar :title="navbarTitle"/>
+    <GridLayout rows="auto, auto">
+      <HelloWorld :msg="msg"/>
+    </GridLayout>
+  </Page>
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from '~/components/HelloWorld';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  const { VUE_APP_MODE, VUE_APP_PLATFORM } = process.env;
+
+  export default {
+    components: {
+      HelloWorld,
+    },
+    data() {
+      return {
+        navbarTitle: `App.vue`,
+        msg: `Mode=${VUE_APP_MODE} and Platform=${VUE_APP_PLATFORM}`,
+      };
+    },
+    methods: {
+    }
+  };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style web>
+  @import '~styles/style-one';
+
+  .w-page {
+    height: 100%;
+    width: 100%;
+  }
+</style>
+<style native>
+  @import '~styles/style-one';
 </style>
